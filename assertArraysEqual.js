42b9e3c37d9451a08734359dda4fd1c30b485515
neethu-ms <msneethu2989@gmail.com>
2020-02-04 22:34:00 +0000

    Added assertFunction for arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length)
+    return false;
+  else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i])
+        return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual,expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertArraysEqual([5,6,7],[5,6,7]);
+assertArraysEqual([5,6,7],[5,-6,7]);
